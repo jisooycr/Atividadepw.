286c9f6f12aed7b8e4226b43f7d35e05644606e3
nicolle gomes <nicolle_matiasg@hotmail.com>
2025-06-27 11:40:43 -0300

    Create esfera.php

---
+++ esfera.php
@@ -0,0 +1,12 @@
+<?php
+define("PI", 3.141592);
+
+$raio = 5;
+
+$area = 4 * PI * $raio * $raio;
+$volume = (4 / 3) * PI * $raio * $raio * $raio;
+
+echo "Raio: $raio <br>";
+echo "Área da esfera: $area <br>";
+echo "Volume da esfera: $volume <br>";
+?>
